25b1f28583dc354f88dab174fd71d2e375f83305
Dimi <dimi@dolarapp.com>
2025-10-19 22:38:30 +0200

    refactor(Map): update map style to carto-voyager for improved visuals

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -28,18 +28,18 @@ export default function Map({ userLocation, routes, paidRoutes, selectedRouteId,
       style: {
         version: 8,
         sources: {
-          'carto-light': {
+          'carto-voyager': {
             type: 'raster',
-            tiles: ['https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'],
+            tiles: ['https://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png'],
             tileSize: 256,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
           }
         },
         layers: [
           {
-            id: 'carto-light-layer',
+            id: 'carto-voyager-layer',
             type: 'raster',
-            source: 'carto-light',
+            source: 'carto-voyager',
             minzoom: 0,
             maxzoom: 22
           }
